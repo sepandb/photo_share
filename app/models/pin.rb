bf2b4e41feb77783ed4b96ff69e59e90aac3878c
Sepand Barkhodaee <sepand.barkhodaee@gmail.com>
2013-09-29 20:06:56 -0400

    Add Images via URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,9 @@
 class Pin < ActiveRecord::Base
   
+    attr_accessible :description, :image, :image_remote_url
+
   has_attached_file :image
 
-  attr_accessible :description, :image
 
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
@@ -13,4 +14,8 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "266x200"}
 
+  def image_remote_url=(url_value)
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
+  end
 end
